35472cd66a3ed29367cc3201f330f4f6c09f5d23
Guilherme <150593535+guilherme23x@users.noreply.github.com>
2024-05-27 18:22:01 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 let elemCaixaIcone = document.querySelector(".container_tooltip");
 
-
 elemCaixaIcone.addEventListener("click", function () {
   elemCaixaIcone.classList.toggle("container_tooltip_aberto");
 });
-
