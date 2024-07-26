c582042b04b04e0a22beb6b250c7c65681060ea2
Guilherme <150593535+guilherme23x@users.noreply.github.com>
2024-07-26 19:25:01 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-let elemCaixaIcone = document.querySelector(".container_tooltip");
-
-elemCaixaIcone.addEventListener("click", function () {
-  elemCaixaIcone.classList.toggle("container_tooltip_aberto");
-});
